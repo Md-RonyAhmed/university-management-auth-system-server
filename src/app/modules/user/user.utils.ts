a49e4d8bc1868164971df97867e8f8980cea20b0
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-06-08 00:31:58 +0600

    [DONE] Setup global error handler and optimized the application

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,16 +1,16 @@
-import { User } from './user.model'
+import { User } from './user.model';
 
 export const findLastUserId = async () => {
   const lastUser = await User.findOne({}, { id: 1, _id: -1 })
     .sort({ createdAt: -1 })
-    .lean()
+    .lean();
 
-  return lastUser?.id
-}
+  return lastUser?.id;
+};
 
 export const generateUserId = async () => {
-  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0') //00000
+  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0'); //00000
   //increment by 1
-  const incrementedId = (Number(currentId) + 1).toString().padStart(5, '0') //00001
-  return incrementedId
-}
+  const incrementedId = (Number(currentId) + 1).toString().padStart(5, '0'); //00001
+  return incrementedId;
+};
