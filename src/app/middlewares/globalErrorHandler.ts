56c6334813ca4cefaa076f373d8910d76bcaf04f
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-06-08 16:15:17 +0600

    [DONE] Setup Zod Validation Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,6 +6,8 @@ import { errorLogger } from '../../shared/logger';
 import handleValidationError from '../../errors/handleValidationError';
 import ApiError from '../../errors/ApiError';
 import { IGenericErrorMessage } from '../../interfaces/error';
+import { ZodError } from 'zod';
+import handleZodError from '../../errors/handleZodError';
 
 const globalErrorHandler: ErrorRequestHandler = (
   error,
@@ -23,6 +25,11 @@ const globalErrorHandler: ErrorRequestHandler = (
 
   if (error?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
     errorMessages = simplifiedError.errorMessages;
---
+++ src/app/middlewares/validateRequest.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject } from 'zod';
+
+const validateRequest =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        params: req.params,
+        query: req.query,
+        headers: req.headers,
+        cookies: req.cookies,
+      });
+      return next();
+    } catch (error) {
+      next(error);
+    }
+  };
+export default validateRequest;
