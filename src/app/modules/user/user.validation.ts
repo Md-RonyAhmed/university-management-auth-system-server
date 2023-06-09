94ddcb6e1674ede7961b7bf6d309e13aeb20aa74
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-06-09 23:35:48 +0600

    [DONE] User Validation Updated

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,8 +2,10 @@ import { z } from 'zod';
 
 const createUserZodSchema = z.object({
   body: z.object({
-    role: z.string({
-      required_error: 'role is required',
+    user: z.object({
+      role: z.string({
+        required_error: 'role is required',
+      }),
     }),
     password: z.string().optional(),
   }),
