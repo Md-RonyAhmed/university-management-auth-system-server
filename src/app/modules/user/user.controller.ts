dcec4122055567071fdab1cfdcbe4447afe9037f
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-06-10 00:23:43 +0600

    [DONE] Optimize Application Routes and Controllers

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,8 +1,8 @@
-import { RequestHandler } from 'express';
+import { Request, Response } from 'express';
 import { UserService } from './user.service';
 import httpStatus from 'http-status';
 
-const createUser: RequestHandler = async (req, res) => {
+const createUser = async (req: Request, res: Response) => {
   const { user } = req.body;
   const data = await UserService.createUser(user);
   res.status(httpStatus.OK).send({
