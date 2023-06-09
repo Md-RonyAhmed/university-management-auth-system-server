7b080db40ee037f0ca970c971fd97bd35410d147
Md-RonyAhmed <rony15-10527@diu.edu.bd>
2023-06-09 20:28:50 +0600

    [DONE] Ceate Semester Code Validation in service layer

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,9 +1,18 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createSemester = async (
   payload: IAcademicSemester
 ): Promise<IAcademicSemester> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Invalid code: Type=> 01-Autumn, 02-Summer, 03-Fall'
+    );
+  }
   const data = await AcademicSemester.create(payload);
   return data;
 };
